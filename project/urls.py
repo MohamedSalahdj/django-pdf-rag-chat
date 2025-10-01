fac9b58a815a34d461724e4d17194da240af71cf
Mohamed Salah <mohamedsamydj@gmail.com>
2025-10-01 19:33:19 +0300

    add media and static file serving in development mode

--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,3 +25,8 @@ urlpatterns = [
     path('api/v1/account/', include('account.urls')),
     path('api/v1/rag/', include('rag.urls')),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
